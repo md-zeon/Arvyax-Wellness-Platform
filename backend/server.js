a3f146adda5b92f5d63da0fa454fd8c62ed71034
md-zeon <zeonrahaman5870@gmail.com>
2025-08-11 09:06:54 +0600

    Initialized Backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Arvyax Wellness Platform API is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
