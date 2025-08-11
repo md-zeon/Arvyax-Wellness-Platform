3850ccd9f394282acad2f3b1b0342381a72f05ec
md-zeon <zeonrahaman5870@gmail.com>
2025-08-11 19:07:09 +0600

    Deployed to vercel

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
 	try {
-		await client.connect();
+		// await client.connect();
 		const database = client.db("arvyaxDB");
 		const usersCollection = database.collection("users");
 		const sessionsCollection = database.collection("sessions");
@@ -319,8 +319,8 @@ async function run() {
 		});
 
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
-		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		// await client.db("admin").command({ ping: 1 });
+		// console.log("Pinged your deployment. You successfully connected to MongoDB!");
 	} finally {
 		// Ensures that the client will close when you finish/error
 		// await client.close();
