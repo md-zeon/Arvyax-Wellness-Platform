91ff836a4909b698b83dc9de168d968597b35d25
md-zeon <zeonrahaman5870@gmail.com>
2025-08-11 09:25:51 +0600

    Connected Backend with MongoDB atlas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,12 +8,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Arvyax Wellness Platform API is running!');
-});
 
 const PORT = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+    console.error("MONGODB_URI is not defined in .env file");
+    process.exit(1);
+}
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const database = client.db("arvyaxDB");
+		const usersCollection = database.collection("users");
+
+
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("Arvyax Wellness Platform API is running!");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
